713eff0abc190e7c4a859fbe545b4d48e3f0c5c3
Tarik Desire <tprdesire@gmail.com>
2018-07-30 15:49:25 -0400

    Imported express library and added static server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+// Load array of notes
+const data = require('./db/notes');
+
+const app = express();
+
+// ADD STATIC SERVER HERE
+
+app.listen(8080, function() {
+  console.log(`Server is listening on ${this.address().port}`);
+}).on('error', err => {
+  console.log(err);
+});
