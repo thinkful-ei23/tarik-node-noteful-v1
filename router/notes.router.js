8721adc4a64aac7a0fd7e4bf76ba2fd33103535e
Tarik Desire <tprdesire@gmail.com>
2018-08-02 14:51:50 -0400

    Refactored simDB and notes.METHODs to use promises

--- router/notes.router.js
+++ router/notes.router.js
@@ -13,22 +13,28 @@ const notes = simDB.initialize(data);
 
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
-  notes.filter(searchTerm, function(err, list) {
-    if (err) {
-      return next(err);
-    }
-    return res.json(list);
-  });
+  notes.filter(searchTerm)
+    .then(list => {
+      return res.json(list);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
   
 router.get('/notes/:id', (req, res, next) => {
   const { id } = req.params;
-  notes.find(id, function(err, item) {
-    if (err) {
-      return next(err);
-    }
-    return res.json(item);
-  });
+  notes.find(id)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
   
 router.put('/notes/:id', (req, res, next) => {
@@ -40,16 +46,17 @@ router.put('/notes/:id', (req, res, next) => {
       updateObj[field] = req.body[field];
     }
   });
-  notes.update(id, updateObj, function(err, item) {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      return res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.update(id, updateObj)
+    .then(item => {
+      if(item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.post('/notes', (req, res, next) => {
@@ -61,27 +68,28 @@ router.post('/notes', (req, res, next) => {
     return next(err);
   }
 
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  notes.create(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.delete('/notes/:id', (req, res) => {
   const { id } = req.params;
-  notes.delete(id, (err) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
+  notes.delete(id)
+    .then(()=>{
       res.sendStatus(204);
-    }
-  });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
