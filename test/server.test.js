e25b7966baf0dd29a94799a5ea6099e3eab2a300
Tarik Desire <tprdesire@gmail.com>
2018-08-03 16:44:50 -0400

    Added test for GET request with invalid id

--- test/server.test.js
+++ test/server.test.js
@@ -121,5 +121,19 @@ describe('Notes Database', function() {
         expect(res.body.title).to.equal(title);
         expect(res.body.content).to.equal(content);
       });
-  }); 
+  });
+  
+  it('should respond with a 404 for an invalid id', function() {
+    return chai.request(app)
+      .get('/api/notes/DOESNOTEXIST')
+      .then(function(res) {
+        expect(res).to.have.status(404);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.all.keys(
+          'message'
+        );
+        expect(res.body.message).to.equal('Not Found');
+      });
+  });
 });
