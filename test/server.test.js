cfa14c264bbac25bc8cac795712b2adbfe46dfcd
Tarik Desire <tprdesire@gmail.com>
2018-08-03 14:58:00 -0400

    Tested static server at root endpoint for for bad request

--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const app = require('../server')
+const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+
+chai.use(chaiHttp);
 
 describe('Reality check', function() {
   it('true should be true', function() {
@@ -12,5 +14,27 @@ describe('Reality check', function() {
 
   it('2 + 2 should equal 4', function() {
     expect(2 + 2).to.equal(4);
+  });
+});
+
+describe('Express static', function() {
+  it('GET request to \'/\' should return the index page', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
+
+describe('404 handler', function() {
+  it('should respond with 404 when given a bad path', function() {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then(function(res) {
+        expect(res).to.have.status(404);
+      });
   });
 });
